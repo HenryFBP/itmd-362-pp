0a0d89c9d2d2eb0a7d8104a055a8dc426baf8f44
HenryFBP <HenryFBP@gmail.com>
2018-01-26 13:47:00 -0600

    add basic email @symbol checking

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,14 +1,42 @@
+log = function(it) {console.log(it);}
+
+function scontains(haystack, needle)
+{
+  log("Seeing if '"+haystack+"' has '"+needle+"' in it.")
+  return (haystack.toString().indexOf(needle) !== -1);
+}
+
+
 $('document').ready(function() {
-  console.log("Doc is ready!")
+  log("Doc is ready!");
   
   
 })
 
+function checkEmail(email)
+{
+  if(scontains(email,"@"))
+  {
+    return true;
+  }
+  return false;
+}
 
 $('#sign-up').on("submit", function(e) {
   e.preventDefault();
   
-  window.alert('Thank you, ' + $('#email').val() + ".")
+  if(checkEmail($('#email').val()) === true)
+  {
+    log("Email OK!");
+  }
+  else
+  {
+    alert("EMAIL NO GOOD AAAA WHER IS UR @ SYMBOL???")
+    return;
+  }
+  
+  
+  alert('Thank you, ' + $('#email').val() + ".")
   
   $('html').append("SUBMITTED?!?!")
 })
